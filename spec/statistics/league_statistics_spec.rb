7e90f2b48694be3463255a0ab04f2217bc7604c6
Ian Ross <ianrossdrums@gmail.com>
2022-01-11 09:05:06 -0700

    save

--- spec/statistics/league_statistics_spec.rb
+++ spec/statistics/league_statistics_spec.rb
@@ -6,12 +6,12 @@ require 'pry'
 
 RSpec.describe LeagueStatistics do
   before(:each) do
-    mock_game_team_1 = OpenStruct.new({ team_id: "3", goals: 3})
-    mock_game_team_2 = OpenStruct.new({ team_id: "6", goals: 5})
-    mock_game_team_3 = OpenStruct.new({ team_id: "3", goals: 5})
-    mock_game_team_4 = OpenStruct.new({ team_id: "6", goals: 5})
-    mock_game_team_5 = OpenStruct.new({ team_id: "2", goals: 2})
-    mock_game_team_6 = OpenStruct.new({ team_id: "2", goals: 1})
+    mock_game_team_1 = OpenStruct.new({ team_id: "3", HoA: "away", goals: 3})
+    mock_game_team_2 = OpenStruct.new({ team_id: "6", HoA: "home", goals: 5})
+    mock_game_team_3 = OpenStruct.new({ team_id: "3", HoA: "home", goals: 9})
+    mock_game_team_4 = OpenStruct.new({ team_id: "6", HoA: "away", goals: 5})
+    mock_game_team_5 = OpenStruct.new({ team_id: "2", HoA: "away", goals: 2})
+    mock_game_team_6 = OpenStruct.new({ team_id: "2", HoA: "home", goals: 1})
     mock_game_team_manager = OpenStruct.new({ data: [mock_game_team_1, mock_game_team_2, mock_game_team_3, mock_game_team_4, mock_game_team_5, mock_game_team_6]})
     @league_statistics = LeagueStatistics.new(mock_game_team_manager)
   end
@@ -24,7 +24,7 @@ RSpec.describe LeagueStatistics do
     end
   end
 
-  describe "find best offense" do
+  describe "find offense" do
     it "puts one teams games into an array" do
       actual = @league_statistics.group_teams
       expect(actual).to be_a(Hash)
@@ -32,7 +32,7 @@ RSpec.describe LeagueStatistics do
 
     it "finds highest average goals overall" do
       actual = @league_statistics.best_offense
-      expected = "6"
+      expected = "3"
       expect(actual).to eq(expected)
     end
 
@@ -41,7 +41,19 @@ RSpec.describe LeagueStatistics do
       expected = "2"
       expect(actual).to eq(expected)
     end
-
   end
 
+  describe "finds HoA avaerages" do
+    it "finds highest average goals per game while away" do
+      actual = @league_statistics.highest_scoring_visitor
+      expected = "6"
+      expect(actual).to eq(expected)
+    end
+
+    it "finds highest average goals per game while home" do
+      actual = @league_statistics.highest_scoring_home_team
+      expected = "6"
+      expect(actual).to eq(expected)
+    end
+  end
 end
